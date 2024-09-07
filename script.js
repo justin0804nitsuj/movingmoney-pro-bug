f86e63fe03eb1cdcce57bfb62dcc46123b2b42bb
justin假斯叮 <jusitnjuju0804@gmail.com>
2024-09-07 23:13:26 +0800

    123

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ fetch('/visit')
         document.getElementById('total-visited').textContent = `來訪人次: ${data.totalVisited}`;
     });
 
-    // Import the functions you need from the SDKs you need
+        // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
