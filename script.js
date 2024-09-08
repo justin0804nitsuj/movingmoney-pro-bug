e3e2eba30e53397da7660d4de98c4aafbc90a1dc
justin假斯叮 <jusitnjuju0804@gmail.com>
2024-09-08 17:24:33 +0800

    完成

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function login() {
+    const username = document.getElementById('username').value;
+    if (username) {
+        // 導向到 main.html 並傳遞使用者名稱
+        window.location.href = `main.html?username=${encodeURIComponent(username)}`;
+    } else {
+        alert("請輸入姓名");
+    }
+}
+
+function getParameterByName(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
 function displayWelcomeMessage() {
     const username = getParameterByName('username');
     if (username) {
